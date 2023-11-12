1763f510d6ff7b99f257099065332b19e79f5db5
kopeckakam <kopecka.kam@gmail.com>
2023-11-12 22:52:14 +0100

    ukol - 05

--- src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceController.java
@@ -1,7 +1,13 @@
 package cz.czechitas.java2webapps.ukol5.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.validation.Valid;
 
 /**
  * Kontroler obsluhující registraci účastníků dětského tábora.
@@ -9,9 +15,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RegistraceController {
 
-  @GetMapping("/")
-  public String formular() {
-    return "formular";
-  }
+    @GetMapping("/")
+    public ModelAndView formular() {
+        ModelAndView modelAndView = new ModelAndView("formular");
+        modelAndView.addObject("form", new RegistraceForm());
+        return modelAndView;
+    }
+
+    @PostMapping("")
+    public Object form(@Valid @ModelAttribute("form") RegistraceForm form, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "formular";
+        }
+
+        return new ModelAndView("rekapitulace");
+    }
 
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol5/controller/RegistraceForm.java
@@ -0,0 +1,96 @@
+package cz.czechitas.java2webapps.ukol5.controller;
+
+import jakarta.validation.constraints.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
+
+public class RegistraceForm {
+
+    @NotBlank
+    private String jmeno;
+    @NotBlank
+    private String prijmeni;
+    @NotNull
+    private LocalDate datumNarozeni;
+    @Min(9)
+    @Max(15)
+    private int vek;
+    @NotBlank
+    private String pohlavi;
+    @NotBlank
+    private String turnus;
+    @Email
+    private String email;
+    @Pattern(regexp = "[\\d]{0,9}")
+    private String telefon;
+
+    public RegistraceForm() {
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public LocalDate getDatumNarozeni() {
+        return datumNarozeni;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public int getVek() {
+        return vek;
+    }
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    public void setDatumNarozeni(LocalDate datumNarozeni) {
+        Period period = datumNarozeni.until(LocalDate.now());
+        this.vek = period.getYears();
+        this.datumNarozeni = datumNarozeni;
+    }
+
+    public void setPrijmeni(String prijmeni) {
+        this.prijmeni = prijmeni;
+    }
+
+    public String getPrijmeni() {
+        return prijmeni;
+    }
+
+    public String getPohlavi() {
+        return pohlavi;
+    }
+
+    public String getTurnus() {
+        return turnus;
+    }
+
+    public void setPohlavi(String pohlavi) {
+        this.pohlavi = pohlavi;
+    }
+
+    public void setTurnus(String turnus) {
+        this.turnus = turnus;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+}
